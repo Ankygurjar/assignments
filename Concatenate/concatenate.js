1c0a938170596bc6c555b5b02a0aa8c307411791
Ankit Kumar <44130267+Ankygurjar@users.noreply.github.com>
2020-02-27 14:58:10 -0500

    firstCommit

---
+++ Concatenate/concatenate.js
@@ -0,0 +1,10 @@
+const name = 'Reza'
+const age = 54
+const num = 2074
+
+console.log(name+":",age)
+console.log(name+age)
+console.log(name+age)
+console.log(num)
+console.log(name+age+2020)
+console.log(age+name+2020)
---
+++ ScriptConsole/console.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const yo = 'Yo.'
+const temp = 21.3
+const arr = ['pomme', 'poire', 'orange']
+const temperatureStr = 'The temperature is'
+const collectionStr = 'The collection of fruits is '
+const html = '<h1>No HTML interpreter</h1>'
+const age = 18
+const age2 = 'eighteen'
+const bool = true
+const bool2 = null
+const groupStr = 'This is a group'
+const groupContent = ' Group Content'
+const secondGroup = 'This is a second group (closed by default)'
+const secondContent = ' Content of the second group'
+
+console.log(yo)
+console.log(temp)
+console.log(arr)
+console.log(temperatureStr,temp)
+console.log(collectionStr, arr)
+console.log(html)
+console.log(age+" "+age2+" "+bool+" "+bool2+" ",undefined)
+console.log(groupStr)
+console.log(groupContent)
+console.log(secondGroup)
+console.log(secondContent)
